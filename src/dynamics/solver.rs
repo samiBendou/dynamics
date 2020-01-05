76892619deee0ed14ec7d9cf93b270adc088f14d
bendou <sami.dahoux@etu.emse.fr>
2020-01-05 16:02:55 +0100

    Clean RK4 method

--- src/dynamics/solver.rs
+++ src/dynamics/solver.rs
@@ -23,52 +23,74 @@ impl Method {
 pub struct Solver {
     pub dt: f64,
     pub method: Method,
-    state: Vector4,
-    tmp: [Vector4; 4],
+    state: Vec<Vector4>,
+    tmp: Vec<[Vector4; 4]>,
 }
 
 impl Solver {
     pub fn new(dt: f64, method: Method) -> Solver {
-        Solver { dt, method, state: Vector4::zeros(), tmp: [Vector4::zeros(); 4] }
+        Solver { dt, method, state: Vec::new(), tmp: Vec::new() }
     }
 
     pub fn step<T>(&mut self, bodies: &mut Vec<Body>, mut f: T, iterations: u32) -> &mut Self where
         T: FnMut(&Vec<Body>, usize) -> Vector4 {
         let len = bodies.len();
-
+        self.tmp = bodies.iter().map(|_body| [Vector4::zeros(); 4]).collect();
+        self.state = bodies.iter().map(|_body| Vector4::zeros()).collect();
 
         for _ in 0..iterations {
-            for i in 0..len {
-                match self.method {
-                    Method::EulerExplicit => self.euler_explicit(bodies, i, &mut f),
-                    Method::RungeKutta4 => self.runge_kutta_4(bodies, i, &mut f),
-                };
-            }
-            for i in 0..len {
-                bodies[i].center.accelerate(self.dt);
+            match self.method {
+                Method::EulerExplicit => self.euler_explicit(bodies, &mut f),
+                Method::RungeKutta4 => self.runge_kutta_4(bodies, &mut f),
+            };
+            for j in 0..bodies.len() {
+                bodies[j].center.accelerate(self.dt);
             }
         }
         self
     }
 
-    pub fn euler_explicit<T>(&mut self, bodies: &mut Vec<Body>, i: usize, f: &mut T) -> &mut Self where
+    pub fn euler_explicit<T>(&mut self, bodies: &mut Vec<Body>, f: &mut T) -> &mut Self where
         T: FnMut(&Vec<Body>, usize) -> Vector4 {
-        bodies[i].center.gradient = f(bodies, i);
+        for j in 0..bodies.len() {
+            bodies[j].center.gradient = f(bodies, j);
+        }
         self
     }
-    pub fn runge_kutta_4<T>(&mut self, bodies: &mut Vec<Body>, i: usize, f: &mut T) -> &mut Self where
+    pub fn runge_kutta_4<T>(&mut self, bodies: &mut Vec<Body>, f: &mut T) -> &mut Self where
         T: FnMut(&Vec<Body>, usize) -> Vector4 {
         let half_dt = 0.5 * self.dt;
-        self.tmp[0] = f(bodies, i);
-        self.state = bodies[i].center.state.vector();
-        bodies[i].center.state.set_vector(&(self.state + self.tmp[0] * half_dt));
-        self.tmp[1] = f(bodies, i);
-        bodies[i].center.state.set_vector(&(self.state + self.tmp[1] * half_dt));
-        self.tmp[2] = f(bodies, i);
-        bodies[i].center.state.set_vector(&(self.state + self.tmp[2] * self.dt));
-        self.tmp[3] = f(bodies, i);
-        bodies[i].center.state.set_vector(&self.state);
-        bodies[i].center.gradient = (self.tmp[0] + (self.tmp[1] + self.tmp[2]) * 2. + self.tmp[3]) * FRAC_1_6;
+        for j in 0..bodies.len() {
+            self.tmp[j][0] = f(bodies, j);
+            bodies[j].center.gradient = self.tmp[j][0];
+        }
+        for j in 0..bodies.len() {
+            self.state[j] = bodies[j].center.state.vector();
+            bodies[j].center.state.set_vector(&(self.state[j] + self.tmp[j][0] * half_dt));
+        }
+
+        for j in 0..bodies.len() {
+            self.tmp[j][1] = f(bodies, j);
+        }
+        for j in 0..bodies.len() {
+            bodies[j].center.gradient += self.tmp[j][1] * 2.;
+            bodies[j].center.state.set_vector(&(self.state[j] + self.tmp[j][1] * half_dt));
+        }
+        for j in 0..bodies.len() {
+            self.tmp[j][2] = f(bodies, j);
+        }
+        for j in 0..bodies.len() {
+            bodies[j].center.gradient += self.tmp[j][2] * 2.;
+            bodies[j].center.state.set_vector(&(self.state[j] + self.tmp[j][2] * self.dt));
+        }
+        for j in 0..bodies.len() {
+            self.tmp[j][3] = f(bodies, j);
+        }
+        for j in 0..bodies.len() {
+            bodies[j].center.gradient += self.tmp[j][3];
+            bodies[j].center.gradient /= FRAC_1_6;
+            bodies[j].center.state.set_vector(&self.state[j]);
+        }
         self
     }
 }
