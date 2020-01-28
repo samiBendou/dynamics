ca0cb5810d5531a45e48e871413ddc90cd3e704d
sami <sami@dahoux.net>
2020-01-28 19:15:08 +0100

    Writing documentation and renaming navier_stokes to quadratic_drag and gravity to newtonian gravity

--- src/forces.rs
+++ src/forces.rs
@@ -1,18 +1,37 @@
+//!
+//! This module provides functions to compute vectors for the commonly used force in point's dynamics
+//!
+//! ## Conventions
+//! * The forces takes dynamics points as parameters as first parameters
+//! * Forces returned are of size 2N for a point with N degrees of freedom
+
 use geomath::prelude::*;
 use geomath::vector::{*, self};
 
 use crate::consts::G_UNIV;
 use crate::point::Point3;
 
-const RESISTANCE: f64 = 0.001;
-
-pub fn nav_stokes(point: &Point3) -> Vector6 {
+/// Simplified quadratic fluid drag.
+///
+/// This is a force that is quadratically opposed to the speed.
+/// You change fluid parameters such as density or change cross sectional area by computing coherent values of
+/// the resistance.
+///
+/// **Note :** The resistance is given by the following equation **R = 0.5 D S C** where
+/// * **D** is the density of the fluid
+/// * **S** is the cross sectional area
+/// * **C** is the drag coefficient
+///
+pub fn quadratic_drag(point: &Point3, resistance: f64) -> Vector6 {
     let speed = point.state.speed.magnitude();
-    let acceleration = point.state.speed * (-RESISTANCE / point.mass * speed);
+    let acceleration = point.state.speed * (-resistance / point.mass * speed);
     Vector6::concat(&point.state.speed, &acceleration)
 }
 
-pub fn gravity(point: &Point3, points: &Vec<Point3>) -> Vector6 {
+/// Newton's n-body gravity attraction
+///
+/// This the sum of the 2-body newtonian forces felt on a specific point surrounded by masses at specific locations.
+pub fn newton_gravity(point: &Point3, points: &Vec<Point3>) -> Vector6 {
     let len = points.len();
     let mut acceleration = vector::consts::ZEROS_3;
     let mut distance: Vector3;
--- src/potentials.rs
+++ src/potentials.rs
@@ -1,7 +1,16 @@
+//!
+//! This module provides functions to compute values for the commonly used potentials in point's dynamics
+//!
+//! ## Conventions
+//! * The potentials takes dynamics points as parameters as first parameters
+
 use crate::consts::G_UNIV;
 use crate::point::Point3;
 
-pub fn gravity(point: &Point3, points: &Vec<Point3>) -> f64 {
+/// Newton's n-body gravitational potential
+///
+/// This the sum of the 2-body newtonian potential felt on a specific point surrounded by masses at specific locations.
+pub fn newton_gravity(point: &Point3, points: &Vec<Point3>) -> f64 {
     let mut ret = 0.;
     let mut distance: f64;
     for i in 0..points.len() {
